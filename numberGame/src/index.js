8bce4449c427a93213bf945dff19bc39da6a5671
mungmung <mulgoms2@gmail.com>
2021-04-29 16:38:02 +0900

    better number game

--- numberGame/src/index.js
+++ numberGame/src/index.js
@@ -1,9 +1,9 @@
 // <⚠️ DONT DELETE THIS ⚠️>
-import "./styles.css";
+// import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 const form = document.querySelector('.js-form'),
   form2 = document.querySelector('.js-form2'),
-  input = form.querySelector('.js-input'),
+  range = form.querySelector('.js-input'),
   input2 = form2.querySelector('.js-input2'),
   h3 = document.querySelector('.js-h3'),
   span = document.querySelector('.js-span'),
@@ -16,44 +16,38 @@ function makeRange(e){
   h3.innerHTML = innerText;
 }
 
-function submitNumber(e){
+function playGame(e){
     e.preventDefault();
-    if (input2.value) {
-        paintWinner();
+    if (input2.value === "") {
+      span.innerHTML = 'choose the number';
+      return;
     }
+    const userNumber = parseInt(input2.value, 10), //decimal radix
+    maxRange = range.value, //Generated number
+    computerNumber = Math.ceil(Math.random() * maxRange); //random method return 0~1, celi return integer
+    
+    span.innerHTML = `You choose: ${userNumber}, the machine choose: ${computerNumber}.<br />
+    <strong>${userNumber === computerNumber ? "You Win!" : "You lose!"}</strong>`;
+
+    // if (userNumber === computerNumber) {
+    //     span.innerHTML = `You choose: ${userNumber}, the machine choose: ${computerNumber}`;
+    //     winner.innerHTML = 'You Win!'; 
+    // } else {
+    //     span.innerHTML = `You choose: ${userNumber}, the machine choose: ${computerNumber}`;
+    //     winner.innerHTML = 'You lose';
+    // }
 }
 
-function paintWinner(){
-    const userNumber = parseInt(input2.value),
-    rangeValue = input.value, //Generated number
-    computerNumber = makeRandomeNumber(rangeValue);
- 
-    if (userNumber === computerNumber) {
-        span.innerHTML = `You choose: ${userNumber}, the machine choose: ${computerNumber}`;
-        winner.innerHTML = 'You Win!'; 
-    } else {
-        span.innerHTML = `You choose: ${userNumber}, the machine choose: ${computerNumber}`;
-        winner.innerHTML = 'You lose';
-    }
-
-}
-
-function makeRandomeNumber(number){
-  const gameRange = getRandomInt(0, number); //make randomNumber with choosed range value
-  return gameRange;
-}
-
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
-  }
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+//   }
 
 
 function init(){
 form.addEventListener('input', makeRange);
-form2.addEventListener('submit', submitNumber);
+form2.addEventListener('submit', playGame);
 }
 
 
